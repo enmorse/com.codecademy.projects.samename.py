74845764687dcb76e50a65b840ad7588f9d13e67
enmorse <enmorse48@gmail.com>
2022-02-11 18:15:15 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Write your same_name function here:
+def same_name(your_name, my_name):
+    return True if your_name == my_name else False
+
+
+print(same_name("Colby", "Colby"))
+# should print True
+print(same_name("Tina", "Amber"))
+# should print False
